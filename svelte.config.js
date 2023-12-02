f926217daa07c6c30d3f6f77133404a233af779a
zxcq544 <zxcq544@yandex.ru>
2023-12-02 17:03:07 +0300

    change path

--- svelte.config.js
+++ svelte.config.js
@@ -14,11 +14,6 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
